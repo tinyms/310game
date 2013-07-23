6820bd80e3f95002c9f2b8c6ca9d13221b74f2da
tinyms <i@tinyms.com>
2013-07-23 11:13:11 +0800

    Signed-off-by: tinyms <i@tinyms.com>

--- postgres.py
+++ postgres.py
@@ -4,13 +4,16 @@ import re
 import psycopg2
 import psycopg2.extras
 
+
 def pg_open():
-    return psycopg2.connect(database="postgres",user="postgres",password="1")
+    return psycopg2.connect(database="tinyms", user="postgres", password="1")
+
 
 def to_floats(odds_text):
     if not odds_text:
         return list()
-    return [round(float(f),2) for f in odds_text.split(" ")]
+    return [round(float(f), 2) for f in odds_text.split(" ")]
+
 
 def wl_lb_match_history_cache(matchs):
     rows = []
@@ -24,7 +27,7 @@ def wl_lb_match_history_cache(matchs):
         arr.append(row["score"])
         arr.append(row["db_actual_result"])
         arr.append(row["result"])
-        arr.append(round(float(row["asia_avg_balls"]),2))
+        arr.append(round(float(row["asia_avg_balls"]), 2))
         arr.append(" VS ".join(row["team_names"]))
         arr.append(row["last_10_text_style"])
         arr.append(row["last_6_text_style"])
@@ -48,7 +51,6 @@ def wl_lb_match_history_cache(matchs):
         arr.append(row["match_date"])
         rows.append(arr)
 
-
     sql = """
             INSERT INTO matchs(score,actual_result,detect_result,balls_diff,vs_team_names,last_10,last_6,last_4,last_battle,
             odds_wl,odds_lb,odds_am,odds_beta,odds_ysb,odds_wl_c,odds_lb_c,odds_am_c,odds_beta_c,odds_ysb_c,wl_lb_flag,wl_lb_diff,
@@ -58,44 +60,46 @@ def wl_lb_match_history_cache(matchs):
     cnn = pg_open()
     cur = cnn.cursor()
     try:
-        cur.executemany(sql,rows)
+        cur.executemany(sql, rows)
         cnn.commit()
         pass
     finally:
         cnn.close()
 
-def query_history_matchs(d_result,flag,match_result,odds_direction,odds_int_num):
 
+def query_history_matchs(d_result, flag, match_result, odds_direction, odds_int_num):
     #odds_direction
     where_extra = ""
     if match_result != "310":
         where_extra += " AND actual_result = %s " % match_result
 
+    if flag:
+        where_extra += " AND wl_lb_flag = %s " % flag
+
     if odds_direction != "-1":
-        int_space = get_number(odds_int_num,0)
+        int_space = get_number(odds_int_num, 0)
         if odds_direction == "3":
             where_extra += " AND odds_wl[1] < odds_wl[3] "
             if int_space >= 1:
-                where_extra += " AND (odds_wl[1]>=%.2f AND odds_wl[1]<=%i)" % (int_space,int(int_space+1))
+                where_extra += " AND (odds_wl[1]>=%.2f AND odds_wl[1]<=%i)" % (int_space, int(int_space + 1))
         elif odds_direction == "0":
             where_extra += " AND odds_wl[1] > odds_wl[3] "
             if int_space >= 1:
-                where_extra += " AND (odds_wl[3]>=%.2f AND odds_wl[3]<=%i)" % (int_space,int(int_space+1))
+                where_extra += " AND (odds_wl[3]>=%.2f AND odds_wl[3]<=%i)" % (int_space, int(int_space + 1))
         elif odds_direction == "1":
             where_extra += " AND odds_wl[1] = odds_wl[3] "
 
-    count_sql = "SELECT COUNT(1) FROM matchs WHERE detect_result = %s AND wl_lb_flag = %s "+where_extra
-    sql = "SELECT * FROM matchs WHERE detect_result = %s AND wl_lb_flag = %s "+where_extra+" ORDER BY random() LIMIT 25";
+    count_sql = "SELECT COUNT(1) FROM matchs WHERE detect_result = %s " + where_extra
+    sql = "SELECT * FROM matchs WHERE detect_result = %s " + where_extra + " ORDER BY random() LIMIT 25";
     print(sql)
     result = dict()
     matchs = list()
     try:
         cnn = pg_open()
         cur = cnn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute(count_sql,(d_result,flag))
+        cur.execute(count_sql, d_result)
         total = cur.fetchone()
-        print(total)
-        cur.execute(sql,(d_result,flag))
+        cur.execute(sql, d_result)
         rows = cur.fetchall()
         for row in rows:
             match = dict()
@@ -103,7 +107,7 @@ def query_history_matchs(d_result,flag,match_result,odds_direction,odds_int_num)
             match["score"] = row["score"]
             match["actual_result"] = row["actual_result"]
             match["detect_result"] = row["detect_result"]
-            match["balls_diff"] = round(float(row["balls_diff"]),2)
+            match["balls_diff"] = round(float(row["balls_diff"]), 2)
             match["vs_team_names"] = row["vs_team_names"]
             match["last_10"] = row["last_10"]
             match["last_6"] = row["last_6"]
@@ -131,17 +135,20 @@ def query_history_matchs(d_result,flag,match_result,odds_direction,odds_int_num)
     result["total"] = total
     return result
 
-def get_number(text,default_ = 0):
+
+def get_number(text, default_=0):
     if not text:
         return default_
     p = re.compile("\\d+[\\.]?\\d+")
     nums = p.findall(text)
-    if len(nums)>0:
+    if len(nums) > 0:
         return float(nums[0])
     return default_
 
+
 def decimals_to_floats(arr):
-    return [round(float(d),2) for d in arr]
+    return [round(float(d), 2) for d in arr]
+
 
 def diff_wl_lb_first_odds(wl, lb):
     rt = {"flag": "", "diff": ""}
@@ -172,13 +179,14 @@ def diff_wl_lb_first_odds(wl, lb):
         rt["flag"] += "N"
 
     items = list()
-    items.append(round(lb_nums[0] - wl_nums[0],2))
-    items.append(round(lb_nums[1] - wl_nums[1],2))
-    items.append(round(lb_nums[2] - wl_nums[2],2))
+    items.append(round(lb_nums[0] - wl_nums[0], 2))
+    items.append(round(lb_nums[1] - wl_nums[1], 2))
+    items.append(round(lb_nums[2] - wl_nums[2], 2))
 
     rt["diff"] = items
 
     return rt
+
 
 def detect_result(match):
     scores = []
