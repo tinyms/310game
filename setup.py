9232b002af44136d631b8c615f96b29e72bdbb1e
tinyms <tinyms.team@gmail.com>
2013-07-31 00:27:33 +0800

    Signed-off-by: tinyms <tinyms.team@gmail.com>

--- setup.py
+++ setup.py
@@ -8,8 +8,15 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 setup(
-        name = "310game",
-        version = "1.0",
-        description = "310game web app",
-        #options = {"build_exe" : {"includes" : ["PyQt5.QtWebKitWidgets","PyQt5.QtNetwork"] }},
-        executables = [Executable("web/server.py", base = base)])
+    name="WaterPress",
+    version="1.0",
+    description="WaterPress for football match game",
+    options={"build_exe": {"includes": ["sip","PyQt5.QtCore","PyQt5.QtGui","psycopg2._psycopg",
+                                        "PyQt5.QtWidgets","PyQt5.QtNetwork","PyQt5.QtOpenGL",
+                                        "PyQt5.QtWebKit","PyQt5.QtPrintSupport","PyQt5.QtXmlPatterns",
+                                        "PyQt5.QtWebKitWidgets"
+                                        ]}},
+    executables=[Executable(script="Main.py",
+                            targetName="WaterPress.exe",
+                            icon= "images/ball.ico",
+                            base=base)])
